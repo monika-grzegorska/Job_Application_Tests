1328fa311cc989e87471a56289a4a7c7b331f029
Monika Grzegorska <monika@grzegorska.pro>
2019-07-26 09:24:48 +0200

    Add project files.

---
+++ JOB_APPLICATION/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOB_APPLICATION
+{
+    public class Class1
+    {
+    }
+}
