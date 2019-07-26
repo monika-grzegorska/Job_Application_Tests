4b981c7ede4b50b5507f6a0e95ca483e93f63c30
Monika Grzegorska <monika@grzegorska.pro>
2019-07-26 18:29:19 +0200

    driver.SwitchTo() working, JobApplicationFormTest -- in Progress

--- JOB_APPLICATION/Pages/JobApplicationForm.cs
+++ JOB_APPLICATION/Pages/JobApplicationForm.cs
@@ -1,25 +1,85 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading.Tasks;
 
 namespace JOB_APPLICATION.Pages
 {
     class JobApplicationForm
     {
         private IWebDriver driver;
+        private WebDriverWait wait;
 
         public JobApplicationForm(IWebDriver driver)
         {
             this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             PageFactory.InitElements(driver, this);
         }
         //-------------
+        [FindsBy(How = How.Id, Using = "Field1")]
+        private IWebElement applyingPositionList; //Required
+        [FindsBy(How = How.Id, Using = "Field2")]
+        private IWebElement firstName;            //Required
+        [FindsBy(How = How.Id, Using = "Field3")]
+        private IWebElement lastName;
+        [FindsBy(How = How.Id, Using = "Field4")]
+        private IWebElement emailAdress;          //Required
 
+        [FindsBy(How = How.Id, Using = "Field5")]
+        private IWebElement firstPartOfPhoneNumber;
+        [FindsBy(How = How.Id, Using = "Field5-1")]
+        private IWebElement secondPartOfPhoneNumber;
+        [FindsBy(How = How.Id, Using = "Field5-2")]
+        private IWebElement thirdPartOfPhoneNumber;
 
+        [FindsBy(How = How.Id, Using = "Field15_0")]
+        private IWebElement relocationRadioButtonYes; //Required
+        [FindsBy(How = How.Id, Using = "Field15_1")]
+        private IWebElement relocationRadioButtonNo; //Required
+
+        [FindsBy(How = How.Id, Using = "Field7")]
+        private IWebElement resumeUploadButton;   //Required
+        [FindsBy(How = How.Id, Using = "Field8")]
+        private IWebElement coverLetterButton;
+
+        [FindsBy(How = How.Id, Using = "Field9")]
+        private IWebElement linkedinURL;
+        [FindsBy(How = How.Id, Using = "Field10")]
+        private IWebElement twitterURL;
+        [FindsBy(How = How.Id, Using = "Field11")]
+        private IWebElement webURL;
+
+        [FindsBy(How = How.Id, Using = "Field12")]
+        private IWebElement aboutField;
+        [FindsBy(How = How.Id, Using = "saveForm")]
+        private IWebElement submitButton;
 
         //-------------
-        public void goToFormPage()
+        public void openFormPage()
         {
             driver.Navigate().GoToUrl("https://www.wufoo.com/gallery/templates/forms/job-application/");
+        }
+
+        public void fillJobFormWithAllInformations()
+        {
+            IWebElement iframe = driver.FindElement(By.Id("wufooFormr11oib2u1w948kb"));
+            driver.SwitchTo().Frame(iframe);
+            new SelectElement(applyingPositionList).SelectByText("Marketing Manager");
+            
+
+            firstName.SendKeys("Monika");
+            lastName.SendKeys("Grzegórska");
+            emailAdress.SendKeys("monika@grzegorska.pro");
+
+            firstPartOfPhoneNumber.SendKeys("531");
+            secondPartOfPhoneNumber.SendKeys("279");
+            thirdPartOfPhoneNumber.SendKeys("284");
+
+            relocationRadioButtonNo.Click();
+
+            resumeUploadButton.SendKeys(@"D:\Monika\Tests\JOB_APPLICATION\JOB_APPLICATION\Monika_Grzegórska_QA (2).pdf");
         }
 
     }
--- JOB_APPLICATION/Test_Cases/JobApplicationFormTests.cs
+++ JOB_APPLICATION/Test_Cases/JobApplicationFormTests.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using JOB_APPLICATION.Pages;
 namespace JOB_APPLICATION.Test_Cases
 {
     class JobApplicationFormTests
     {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+        [Test]
+        public void JOB_APPLICATION_Given_GoSignUpPage_When_ClickOnSignUpButton_Then_OpenSignUpPage()
+        {
+            //ARRANGE
+            JobApplicationForm jobForm = new JobApplicationForm(driver);
+            jobForm.openFormPage();
+            //ACT
+            jobForm.fillJobFormWithAllInformations();
+        }
     }
 }
